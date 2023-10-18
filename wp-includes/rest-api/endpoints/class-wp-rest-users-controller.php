16fe49d08e66ada3be0f3e75487729bf170c7a09
tuyenln <tuyenlnse@gmail.com>
2023-10-18 20:51:10 +0700

    update source

--- wp-includes/rest-api/endpoints/class-wp-rest-users-controller.php
+++ wp-includes/rest-api/endpoints/class-wp-rest-users-controller.php
@@ -683,10 +683,8 @@ class WP_REST_Users_Controller extends WP_REST_Controller {
 
 			$request_params = array_keys( $request->get_params() );
 			sort( $request_params );
-			/*
-			 * If only 'id' and 'roles' are specified (we are only trying to
-			 * edit roles), then only the 'promote_user' cap is required.
-			 */
+			// If only 'id' and 'roles' are specified (we are only trying to
+			// edit roles), then only the 'promote_user' cap is required.
 			if ( array( 'id', 'roles' ) === $request_params ) {
 				return true;
 			}
@@ -1307,7 +1305,7 @@ class WP_REST_Users_Controller extends WP_REST_Controller {
 			);
 		}
 
-		if ( str_contains( $password, '\\' ) ) {
+		if ( false !== strpos( $password, '\\' ) ) {
 			return new WP_Error(
 				'rest_user_invalid_password',
 				sprintf(
